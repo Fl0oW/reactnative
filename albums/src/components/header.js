aa75b2be8558580eba072c4c2379dde6936aceef
Fl0oW <flowspammail@yahoo.de>
2017-01-31 17:29:25 -0500

    New Comment
    Added a new comment for test reasons

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,1 +1,12 @@
-header.js
+// Import libraries for making a component
+import React from 'react';
+import { Text } from 'react-native';
+
+
+// Make a component
+const Header = () => {
+	return <Text>Albums!</Text>;
+};
+
+// Make a component available to other parts of the app
+// New comment
